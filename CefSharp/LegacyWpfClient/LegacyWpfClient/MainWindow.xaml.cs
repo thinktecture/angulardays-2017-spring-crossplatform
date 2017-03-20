916c5d3debc52e69d3579ab8cadb7eee7f86f229
Christian Weyer <christian.weyer@thinktecture.com>
2017-03-20 15:23:39 +0100

    Updating CefSharp client

---
+++ CefSharp/LegacyWpfClient/LegacyWpfClient/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace LegacyWpfClient
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private ChromiumWebBrowser webBrowser;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      Cef.Initialize(new CefSettings { CachePath = @".\cachepath" });
+
+      var urlToNavigate = AppDomain.CurrentDomain.BaseDirectory + @"client\index.html";
+      var browserSettings = new BrowserSettings
+      {
+        FileAccessFromFileUrls = CefState.Enabled,
+        WebSecurity = CefState.Enabled
+      };
+
+      webBrowser = new ChromiumWebBrowser();
+      webBrowser.Address = urlToNavigate;
+      webBrowser.BrowserSettings = browserSettings;
+
+      CefSharpContainer.Children.Add(webBrowser);
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.I &&
+          (Keyboard.Modifiers & (ModifierKeys.Control | ModifierKeys.Shift)) == (ModifierKeys.Control | ModifierKeys.Shift))
+      {
+        webBrowser.ShowDevTools();
+      }
+    }
+
+    private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+    {
+      webBrowser.ShowDevTools();
+    }
+  }
+}
